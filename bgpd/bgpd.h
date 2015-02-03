85a8cdf9e8a70dd7c1a3ce1ce9fd6f1f06820f75
Dinesh G Dutt <ddutt@cumulusnetworks.com>
2015-02-03 12:50:33 -0800

    bgpd: Add a no-as-set option to multipath-relax
    Don't announce the AS-SET in routes with as-path multipath-relax.
    
    Signed-off-by: Daniel Walton <dwalton@cumulusnetworks.com>
    Reviewed-by: Vipin Kumar <vipin@cumulusnetworks.com>

--- bgpd/bgpd.h
+++ bgpd/bgpd.h
@@ -176,6 +176,7 @@ struct bgp
 #define BGP_FLAG_RR_ALLOW_OUTBOUND_POLICY (1 << 15)
 #define BGP_FLAG_FORCE_STATIC_PROCESS     (1 << 16)
 #define BGP_FLAG_IMPORT_CHECK_EXACT_MATCH (1 << 17)
+#define BGP_FLAG_MULTIPATH_RELAX_NO_AS_SET (1 << 18)
 
   /* BGP Per AF flags */
   u_int16_t af_flags[AFI_MAX][SAFI_MAX];
