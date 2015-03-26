6438c6c6be234b93e26b002ff2fa434fa3c8f59e
Dinesh G Dutt <ddutt@cumulusnetworks.com>
2015-03-26 13:12:53 -0700

    zebra: zebra-nht-routemap.patch
    Add ability to filter next-hop tracking validation based on route-maps
    
    It is sometimes useful to restrict the resolution of recursive routes
    to only specific via's. For example, in some configurations resolving
    a route through a default route is not acceptable.
    
    This patch adds a new route-map attach point, to zebra's next-hop-tracking
    server. Whenever NHT is considering sending notification of a route
    resolution, it applies a specified route-map and only if it passes, is the
    NHT reachable message sent to the appropriate client protocol (BGP, OSPF etc.).
    If the route-map filters the resolution, then a withdraw is sent to the
    client protocol.
    
    The route-map is sent the ip address of the route via which the resolution is
    happening as well as the valid NHs associated with that route.
    
    We also add support for matching on IP addr prefix len and source protocol
    to ensure that resolution happens only via a very specific route.
    
    Signed-off-by: Dinesh G Dutt <ddutt@cumulusnetworks.com>
    Reviewed-by:   Daniel Walton <dwalton@cumulusnetworks.com>
    Reviewed-by:   Vipin Kumar <vipin@cumulusnetworks.com>

--- zebra/zebra_rnh.h
+++ zebra/zebra_rnh.h
@@ -34,6 +34,7 @@ struct rnh
   struct list *client_list;
   struct prefix resolved_route;
   struct route_node *node;
+  int filtered[ZEBRA_ROUTE_MAX]; /* if this has been filtered for client */
 };
 
 extern int zebra_rnh_ip_default_route;
@@ -44,7 +45,7 @@ extern struct rnh *zebra_lookup_rnh(struct prefix *p, u_int32_t vrfid);
 extern void zebra_delete_rnh(struct rnh *rnh);
 extern void zebra_add_rnh_client(struct rnh *rnh, struct zserv *client);
 extern void zebra_remove_rnh_client(struct rnh *rnh, struct zserv *client);
-extern int zebra_evaluate_rnh_table(int vrfid, int family);
+extern int zebra_evaluate_rnh_table(int vrfid, int family, int force);
 extern int zebra_dispatch_rnh_table(int vrfid, int family, struct zserv *cl);
 extern void zebra_print_rnh_table(int vrfid, int family, struct vty *vty);
 extern char *rnh_str(struct rnh *rnh, char *buf, int size);
